7060d5cc220fe1b22bc9543dfbc831aed389373c
JacksonKnew <paullestrat.hotmail.fr>
2023-06-08 15:54:45 +0200

    getting better at tests

--- tests/test_control.py
+++ tests/test_control.py
@@ -1,5 +1,5 @@
 from email_cleaning_service.control import EmailCleaner
-from email_cleaning_service.utils.request_classes import PipelineSpecs
+from email_cleaning_service.utils.request_classes import PipelineSpecs, RunSpecs
 import pytest
 
 def test_segmenting_service():
@@ -21,3 +21,27 @@ def test_segmenting_service():
 
     dataset = emailCleaner.segment(thread_list, pipeline_specs)
     assert len(dataset.threads) == 2
+
+def test_classifier_training_service():
+    tracking_uri = "https://mentis.io/mlflow/"
+
+    emailCleaner = EmailCleaner()
+
+    # Example of how to train a classifier
+
+    dataset = RunSpecs(
+        run_name="test_run",
+        csv_path="./tests/test_data/test_en.csv",
+        metrics=["seq_f1", "frag_f1"],
+        lr=0.01,
+        epochs=1,
+    )
+
+    pipeline_specs = PipelineSpecs(
+        origin="hugg",
+        classifier_id="a1f66311816e417cb94db7c2457b25d1"
+    )
+
+    emailCleaner.train_classifier(dataset, pipeline_specs)
+    assert True
+
