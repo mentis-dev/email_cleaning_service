98cacb7668fd6b288205ae85fcaa60cd6ca8e9e2
JacksonKnew <paullestrat.hotmail.fr>
2023-06-16 12:19:08 +0200

    added start of azure service

---
+++ email_cleaning_service/services/azure_service.py
@@ -0,0 +1,14 @@
+import azure.storage.blob as azureblob
+
+
+
+class AzureService:
+
+    def __init__(self):
+        pass
+
+    def get_blob(self, blob_name: str): 
+        pass
+
+
+azure_service = AzureService()
